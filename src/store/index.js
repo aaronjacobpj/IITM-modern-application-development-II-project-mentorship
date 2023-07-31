e59559897703789590e56e77dcdf1d98a4d59a95
AARON JACOB <aaronjacob@AARONs-MacBook-Air.local>
2023-07-31 22:42:22 +0530

    added week 3 content

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,9 @@ import { createStore } from "vuex";
 const store = createStore({
     state(){
         return{
-            weeks: [{title: "Vue"},
-                {title: "Vue Routing, Store, RBAC and Token Authentication"},
-                {title: "Backend Jobs and Caching"}
+            weeks: [{title: "Vue", slides: true, source_codes: true},
+                {title: "Vue Routing, Store, RBAC and Token Authentication", slides: true, source_codes: true},
+                {title: "Backend Jobs and Caching", slides: false, source_codes: true}
             ]
         }
     }
--- src/views/WeeksView.vue
+++ src/views/WeeksView.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="container-fluid" style="padding:2% 2%;">
         <h2>
-            {{ $store.state.weeks[$route.params.nweek]["title"] }}
+            {{ week["title"] }}
         </h2>
         <ul style="padding-top: 1%;">
             <li >
@@ -14,12 +14,12 @@
                     Notes
                 </RouterLink>
             </li>
-            <li>
+            <li v-if="week['slides']">
                 <a v-bind:href="`/weeks/slides/week${$route.params.nweek}.pdf`" @click="dowload_file">
                     Slides
                 </a>
             </li>
-            <li>
+            <li v-if="week['source_codes']">
                 <a v-bind:href="`/weeks/source_codes/week${$route.params.nweek}.zip`" @click="dowload_file">
                     Source Code
                 </a>
@@ -31,13 +31,17 @@
     export default {
         data(){
             return {
-            
             }
         },
         methods: {
             dowload_file(event){
                 event.preventDefault();
                 open(event.target.href);
+            }
+        },
+        computed: {
+            week(){
+                return this.$store.state.weeks[this.$route.params.nweek]
             }
         }
     }
